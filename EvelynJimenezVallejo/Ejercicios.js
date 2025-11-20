2969294c91cf9b6b0421e926a6637a1c74edb0d3
evejiva12-hue <evejiva12@gmail.com>
2025-11-20 16:45:23 -0500

    Desafio final

--- EvelynJimenezVallejo/Ejercicios.js
+++ EvelynJimenezVallejo/Ejercicios.js
@@ -139,6 +139,16 @@ let tablero = ["Inicio", null, null, null, null, "Fin"]
 tablero.fill("Casilla Vacía",1,5)
 console.log(tablero)
 
+// 20.Desafío Final 
+let codigos = ["A-10", "C-30", "B-20", "D-40"]
+codigos.sort()
+console.log(codigos)
+codigos.splice(2,1)
+console.log(codigos)
+codigos.unshift("X-99")
+console.log(codigos)
+codigos.reverse()
+console.log(codigos)
 
 
 
