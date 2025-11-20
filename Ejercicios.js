fc419f57bc8abe5f484915384b75d8d24dbbe789
evejiva12-hue <evejiva12@gmail.com>
2025-11-20 13:52:41 -0500

    Pila tareas pendientes

---
+++ Ejercicios.js
@@ -0,0 +1,19 @@
+/*1. Lista Supermercado
+let compras = ["manzanaz"]
+console.log(compras)
+//agregar al final 
+compras.push("leche", "pan")
+//agregar al inicio
+compras.unshift("huevos")
+//eliminar(posicion, cantidad a eliminar)
+compras.splice(1,1)
+console.log(compras)*/
+
+
+//2. Pila de Tareas Pendientes (LIFO)
+let tareas = ["Revisar emails"]
+tareas.unshift("Llamar al cliente")
+tareas.unshift("Preparar reporte")
+tareas.splice(0,1) 
+tareas.spliceÇ(0,1)
+console.log(tareas)
